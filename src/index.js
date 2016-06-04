63006d0650ff881a4ebc748c949b779eefb6acfd
Harrison Harnisch <hharnisc@gmail.com>
2016-06-03 21:08:32 -0500

    First implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-const index = () => true;
+import requestPromise from 'request-promise';
+import retry from 'retry-promise';
+
+const index = (requestOptions = {}, retryOptions = {}) => {
+  if (!requestOptions.uri) {
+    throw new Error('Expecting uri parameter in requestOptions');
+  }
+  const defaultRequestOptions = {
+    method: 'GET',
+    json: true,
+    resolveWithFullResponse: true,
+  };
+  const completeRequestOptions = Object.assign({}, defaultRequestOptions, requestOptions);
+  const defaultRetryOptions = {
+    max: 5,
+    backoff: 1000,
+  };
+  const completeRetryOptions = Object.assign({}, defaultRetryOptions, retryOptions);
+  const retryableRequest = () => requestPromise(completeRequestOptions);
+  return retry(completeRetryOptions, retryableRequest);
+};
 
 export default index;
--- test/index.js
+++ test/index.js
@@ -1,7 +1,50 @@
 import test from 'tape';
-import index from '../src/index';
+import tapSpec from 'tap-spec';
+import proxyquire from 'proxyquire';
+import sinon from 'sinon';
 
-test('request-retry-promise tests', (t) => {
-  t.equal(index(), true, 'succeeds');
+test.createStream()
+  .pipe(tapSpec())
+  .pipe(process.stdout);
+
+
+const setup = () => {
+  const requestSpy = sinon.stub();
+  const retrySpy = sinon.stub();
+  return {
+    requestSpy,
+    retrySpy,
+    sut: proxyquire('../src/index', {
+      'request-promise': requestSpy,
+      'retry-promise': {
+        default: (options, retryable) => {
+          retrySpy(options, retryable);
+          retryable();
+        },
+      },
+    }).default,
+  };
+};
+
+test('Called With Default Arguments', (t) => {
+  const { requestSpy, retrySpy, sut } = setup();
+  const uri = 'http://google.com';
+  sut({ uri });
+  t.equal(true, retrySpy.calledWith({
+    max: 5,
+    backoff: 1000,
+  }), 'retryPromise is called with expected args');
+  t.equal(true, requestSpy.calledWith({
+    method: 'GET',
+    uri,
+    json: true,
+    resolveWithFullResponse: true,
+  }), 'requestPromise is called with expected args');
+  t.end();
+});
+
+test('Explodes when missing uri', (t) => {
+  const { sut } = setup();
+  t.throws(() => (sut()), 'boom!');
   t.end();
 });
